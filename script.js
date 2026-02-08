0be787f19e2abc825271fcd8e85647805575e47d
Dfgug <kirillgubrienko670@gmail.com>
2026-02-08 22:34:35 +0200

    Add user interaction using JS events

--- script.js
+++ script.js
@@ -3,31 +3,34 @@ console.log("JS pieslēgts un darbojas");
 const button = document.getElementById("button");
 const input = document.getElementById("input");
 const article = document.querySelector("article");
+const headerTitle = document.querySelector("header h1");
+
+headerTitle.textContent = "Wikipedia fragments projekts";
+headerTitle.style.color = "yellow";
+headerTitle.style.fontSize = "28px";
 
 const originalText = article.innerHTML;
 
-
 button.addEventListener("click", function () {
+    const value = input.value.trim();
 
-    const searchText = input.value.trim();
-
-    if (searchText === "") {
-        alert("Ievadi meklējamo vārdu!");
-        return;
-    }
+    if (value === "") return;
 
     article.innerHTML = originalText;
 
-    const regex = new RegExp(searchText, "gi");
-
-    if (regex.test(article.innerHTML)) {
-        article.innerHTML = article.innerHTML.replace(regex, `<mark>$&</mark>`);
-    } else {
-        alert("Vārds netika atrasts!");
-    }
+    const regex = new RegExp(value, "gi");
+    article.innerHTML = article.innerHTML.replace(regex, "<mark>$&</mark>");
 });
-
 
 article.addEventListener("dblclick", function () {
     article.classList.toggle("reading-mode");
+});
+
+const clearBtn = document.createElement("button");
+clearBtn.textContent = "Notīrīt izcelšanu";
+document.querySelector("main").appendChild(clearBtn);
+
+clearBtn.addEventListener("click", function () {
+    article.innerHTML = originalText;
+    input.value = "";
 });
